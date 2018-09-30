896772e43fef2a1f0d1e9fda324b060cb7fb83bb
elijah0107 <rgaev@mail.ru>
2018-09-30 22:39:08 +0500

    сборка webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+
+$('.title').html('Text!');
+console.log(111111111111);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+let conf = {
+    entry: '.src/index.js',
+    output: {
+        path: './dist/',
+        filename: 'main.js'
+    }
+};
+
+module.exports = conf;

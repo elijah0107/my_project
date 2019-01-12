110a5376c988d6a7a5b9baaa453dece4746391c8
roman <gayov_ra@ekb.sima-land.ru>
2019-01-12 13:52:41 +0500

    Добавил вид и коллекцию

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
+import Backbone from 'backbone';
 import $ from 'jquery';
+var Item = Backbone.Model.extend({
 
-$('.title').html('Text!');
-console.log(111211111111111111);
+});
+// console.log(new Item);
+// var ItemView = Backbone.View.extend({
+//     initialize: function () {
+//         console.log(this.model)
+//     },
+//     tagName: 'ul',
+// });
+// console.log(new ItemView({model: Item}));
 
+var PeopleCollection = Backbone.Collection.extend({
+    model: Item,
+});
+//Вью для коллекции
+var ItemView = Backbone.View.extend({
+    tagName: '.item',
+
+    initialize: function () {
+
+    },
+
+    render: function () {
+        this.$el.html(this.template({ collection: this.collection. }))
+    }
+});
+var ItemCollection = new PeopleCollection([
+    {
+        name: 'Ножницы',
+        price: 125,
+        description: 'Портновские ножницы',
+    },
+    {
+        name: 'Фоторамка',
+        price: 1200,
+        description: 'Цифровая рамка',
+    },
+    {
+        name: 'Скотч',
+        price: 299,
+        description: 'Прозрачный ',
+    }
+]);
 import './styles.scss';

0b668804c273fb155daf9d15893b7b7cb0c44757
elijah0107 <rgaev@mail.ru>
2019-02-12 23:16:47 +0500

    Добавил поиск

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ $(function () {
             return response.items;
         },
         initialize: function () {
-            var self = this;
-            if (this.get('sid') !== '') {
-                self.set();
-            }
+            // var self = this;
+            // if (this.get('sid') !== '') {
+            //     self.set();
+            // }
         }
     });
 
@@ -33,85 +33,46 @@ $(function () {
     //Вью для коллекции
     App.Views.ItemsList = Backbone.View.extend({
         initialize: function () {
-            // this.itemTemplate = template($('#item-template').html());
-            // this.render();
-            // this.collection.fetch();
-            // console.log(this.collection);
-            // this.on('change:searchSid', this.filterBySid, this);
-            // this.collection.on('reset', this.render, this)
         },
         render: function () {
         },
         events: {
             'click #search-button': 'searchSid'
         },
-
-        searchSid: function (e) {
-            // this.searchSid = e.target.value;
-            // this.trigger('change:searchSid');
-        },
-
-        filterBySid: function () {
-            // this.collection.reset(this.collection.models, {silent: true});
-            // let valueInput = $('.text-value').val();
-            // var searchBySid = this.searchSid,
-            //     filtered = _.filter(this.collection.models, function (item) {
-            //         if (valueInput == item.get('sid')) {
-            //             return String(item.get('sid')).toLowerCase().indexOf(searchBySid.toLowerCase()) !== -1
-            //         }
-            //     });
-            // this.collection.reset(filtered);
-            // this.$('.search-list').html('');
-            // this.collection.each(model => {
-            //     const template = this.itemTemplate(model.toJSON());
-            //     this.$('.search-list').append(template);
-            // })
-        }
     });
 
     App.Views.SearchForm = Backbone.View.extend({
         initialize: function () {
-            this.itemTemplate = template($('#item-template').html());
             this.render();
-            this.on('change:searchSid', this.editSearch, this);
-            this.collection.on('reset', this.render, this)
+            // this.collection.on('reset', this.render, this)
         },
         events: {
-            'click #first-search': 'searchSid'
-        },
-        searchSid: function (e) {
-            this.searchSid = e.target.value;
-            this.trigger('change:searchSid');
+            'click #first-search': 'editSearch'
         },
 
         editSearch: function () {
             let valueInput = $('.text-value').val();
-            this.collection.reset();
-            this.collection.fetch({
-                data: {
-                sids: valueInput
-                }
-            });
-            console.log(this.collection);
+
             if (valueInput !== '') {
+                this.collection.reset();
+                this.render();
+                this.collection.fetch({
+                    data: {
+                        sids: valueInput
+                    }
+                });
                 $('.search-sid').addClass('not-display');
                 $('.search-result').removeClass('not-display');
 
-                this.collection.reset(this.collection.models, {silent: true});
-                let valueInput = $('.text-value').val();
-                var searchBySid = this.searchSid,
-                    filtered = _.filter(this.collection.models, function (item) {
-                        if (valueInput == item.get('sid')) {
-                            console.log(item.get('name'));
-                            return String(item.get('sid')).toLowerCase().indexOf(searchBySid.toLowerCase()) !== -1;
-                        }
-                    });
-                this.collection.reset(filtered);
+                this.itemTemplate = template($('#item-template').html());
                 this.$('.search-list').html('');
-                this.collection.each(model => {
-                    const template = this.itemTemplate(model.toJSON());
-                    this.$('.search-list').append(template);
-                })
+                this.listenTo(this.collection, 'sync', function () {
+                    this.$('.search-list').html('');
+                    this.collection.each(model => {
+                        const template = this.itemTemplate(model.toJSON());
+                        this.$('.search-list').append(template);
+                    })
+                });
             }
         }
     });
@@ -124,7 +85,7 @@ $(function () {
     });
 
     let search = new App.Views.SearchForm({
-       el: '.searchNew',
+        el: '.searchNew',
         collection: items
     });
 });

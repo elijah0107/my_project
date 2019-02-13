a369083aaae6ef65d798a487104db94c46de24a1
elijah0107 <38666717+elijah0107@users.noreply.github.com>
2019-02-13 12:18:06 +0500

    Delete searchView.js

--- src/searchView.js
+++
@@ -1,7 +0,0 @@
-import Backbone from 'backbone';
-import template from 'lodash/template';
-import $ from 'jquery';
-import lodash from 'lodash';
-window._ = lodash;
-
-App.Models.Search = Backbone.Model.extend();

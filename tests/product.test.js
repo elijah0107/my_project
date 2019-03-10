f41a47af61a0f882f80473faf182a1055dfe4d17
elijah0107 <rgaev@mail.ru>
2019-03-10 22:55:06 +0500

    добавил несколько проверок

---
+++ tests/product.test.js
@@ -0,0 +1,5 @@
+import SearchForm from '../src/components/search-form';
+import Items from "../src/components/items";
+import { create as createFixtureLoader } from '../node_modules/fixture-loader';
+import $ from "jquery";
+import Backbone from "backbone";
--- tests/search.test.js
+++ tests/search.test.js
@@ -1,12 +1,16 @@
 import SearchForm from '../src/components/search-form';
 import Items from "../src/components/items";
-import Loader from 'fixture-loader';
+import { create as createFixtureLoader } from '../node_modules/fixture-loader';
 import $ from "jquery";
+import Backbone from "backbone";
+
+
 
 
     describe('check search form', () => {
-        beforeEach(() => {
-            getFixturePath('../index.html');
+        beforeEach(function () {
+            const fixture_loader = createFixtureLoader(__dirname, '../src/')
+            fixture_loader.loadParsedXml('fixtures.html')
         });
          let form,
             createSearchForm = () => {
@@ -23,25 +27,38 @@ import $ from "jquery";
         it('parse test', () => {
 
             const result = items.parse({
-                    items: 'Stone underworld'
+                    items: 'Stone'
             });
-            expect(result).toEqual('Stone underworld');
+            expect(result).toEqual('Stone');
         });
 
-        fit('check edit search', () => {
-            let fixture = Loader('index', 'html');
+        it('check edit search', () => {
             form = createSearchForm();
             expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
             console.log(form.$('.text-value').length);
             form.$('.text-value').val(123456);
-
-            expect(form.$('.text-value').val()).toEqual(123456);
-
+            form.$('.button-blue').click();
             form.editSearch();
             expect(form.editSearch).toHaveBeenCalledTimes(1);
             expect(form.$('.search-result').hasClass('not-display')).toEqual(true);
             expect(form.$('.search-sid').hasClass('not-display')).toEqual(true);
+        });
 
+        it('check open details', () => {
+            form = createSearchForm();
+            form.openDetails();
+            listenTo(Backbone, 'on-click-more-button', function ({ sid }) {
+                console.log(sid);
+            });
+        });
+
+        it('check back to first view', () => {
+            form = createSearchForm();
+            form.backToFirstView();
+            expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
+            expect(form.$('.search-result').hasClass('not-display')).toEqual(true);
+            expect(form.$('.cart-item').hasClass('not-display')).toEqual(true);
+            expect(form.$('.text-value').val()).toEqual('');
         });
 
     });

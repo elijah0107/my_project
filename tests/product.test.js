5ade6ea442e95b23fa744ae7675dd90512e2d455
elijah0107 <rgaev@mail.ru>
2019-03-11 23:26:43 +0500

    Еще несколько проверок

--- tests/product.test.js
+++ tests/product.test.js
@@ -1,4 +1,42 @@
-import SearchForm from '../src/components/search-form';
-import Items from "../src/components/items";
+import ProductView from '../src/components/product-view';
+import CartsItem from "../src/components/carts-item";
 import $ from "jquery";
 import Backbone from "backbone";
+import Load from "../fixture-loader";
+
+describe('check search form', () => {
+    let fixtures = Load('index', 'html'),
+        view,
+        createProductView = () => {
+            return new ProductView({
+                el: '.cart-item',
+                collection: new CartsItem(),
+            });
+        };
+    beforeEach(function () {
+        $('body').append(fixtures);
+    });
+    afterEach(function () {
+        $('body').html('');
+    });
+
+    it('check open characteristic', () => {
+        view = createProductView();
+
+        view.openCharacteristic();
+        expect(view.$('.description-value').hasClass('not-display')).toEqual(true);
+        expect(view.$('.characteristic-value').hasClass('not-display')).toEqual(false);
+        expect(view.$('.characteristic').hasClass('selected')).toEqual(true);
+        expect(view.$('.description').hasClass('selected')).toEqual(false);
+    });
+
+    fit('check open description', () => {
+        view = createProductView();
+
+        view.openDescription();
+        expect(view.$('.description-value').hasClass('not-display')).toEqual(false);
+        expect(view.$('.characteristic-value').hasClass('not-display')).toEqual(false);
+        expect(view.$('.characteristic').hasClass('selected')).toEqual(false);
+        expect(view.$('.description').hasClass('selected')).toEqual(true);
+    });
+});
--- tests/search.test.js
+++ tests/search.test.js
@@ -5,63 +5,60 @@ import $ from "jquery";
 import Backbone from "backbone";
 
 
+describe('check search form', () => {
+    let fixtures = Load('index', 'html');
 
-
-    describe('check search form', () => {
-        let fixtures = Load('index', 'html');
-
-        beforeEach(function () {
-            $('body').append(fixtures);
-        });
-        afterEach(function () {
-            $('body').html('');
-        });
-         let form,
-            createSearchForm = () => {
-                return new SearchForm({
-                    el: '.searchNew',
-                    collection: new Items()
-                });
-         };
-
-        it('check url', function () {
-            const items = new Items();
-            expect(items.url).toEqual('http://www.sima-land.ru/api/v3/search');
-        });
-
-        it('parse test', () => {
-            const items = new Items();
-            const result = items.parse({
-                    items: 'Stone'
-            });
-            expect(result).toEqual('Stone');
-        });
-
-        it('check edit search', () => {
-            form = createSearchForm();
-            expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
-            form.$('.text-value').val(123456);
-            console.log(form.$('.text-value').length);
-            form.editSearch();
-            expect(form.$('.search-result').hasClass('not-display')).toEqual(false);
-            expect(form.$('.search-sid').hasClass('not-display')).toEqual(true);
-        });
-
-        it('check open details', () => {
-            form = createSearchForm();
-            form.openDetails();
-            listenTo(Backbone, 'on-click-more-button', function ({ sid }) {
-                console.log(sid);
-            });
-        });
-
-        it('check back to first view', () => {
-            form = createSearchForm();
-            form.backToFirstView();
-            expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
-            expect(form.$('.search-result').hasClass('not-display')).toEqual(true);
-            expect($('.cart-item').hasClass('not-display')).toEqual(true);
-            expect(form.$('.text-value').val()).toEqual('');
-        });
-
+    beforeEach(function () {
+        $('body').append(fixtures);
     });
+    afterEach(function () {
+        $('body').html('');
+    });
+    let form,
+        createSearchForm = () => {
+            return new SearchForm({
+                el: '.searchNew',
+                collection: new Items()
+            });
+        };
+
+    it('check url', function () {
+        const items = new Items();
+        expect(items.url).toEqual('http://www.sima-land.ru/api/v3/search');
+    });
+
+    it('parse test', () => {
+        const items = new Items();
+        const result = items.parse({
+            items: 'Stone'
+        });
+        expect(result).toEqual('Stone');
+    });
+
+    it('check edit search', () => {
+        form = createSearchForm();
+        expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
+        form.$('.text-value').val(123456);
+        form.editSearch();
+        expect(form.$('.search-result').hasClass('not-display')).toEqual(false);
+        expect(form.$('.search-sid').hasClass('not-display')).toEqual(true);
+    });
+
+    it('check open details', () => {
+        form = createSearchForm();
+        form.openDetails();
+        listenTo(Backbone, 'on-click-more-button', function ({sid}) {
+            console.log(sid);
+        });
+    });
+
+    it('check back to first view', () => {
+        form = createSearchForm();
+        form.backToFirstView();
+        expect(form.$('.search-sid').hasClass('not-display')).toEqual(false);
+        expect(form.$('.search-result').hasClass('not-display')).toEqual(true);
+        expect($('.cart-item').hasClass('not-display')).toEqual(true);
+        expect(form.$('.text-value').val()).toEqual('');
+    });
+
+});

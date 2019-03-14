8ddbab2d274dece24b4f3befc14972ccdcaa3afa
gayov_ra <gayov_ra@ekb.sima-land.ru>
2019-03-14 18:03:58 +0500

    Поправлены имена классов

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import SearchForm from './components/search-form';
 import CartsItem from './components/carts-item';
 import ProductView from './components/product-view';
 
-
-const items = new Items();
-
-const cartsItem = new CartsItem();
-
-const search = new SearchForm({
-  el: '.searchNew',
-  collection: items,
-});
-
-const cart = new ProductView({
-  el: '.cart-item',
-  collection: cartsItem,
-});
+window.app = {
+  searchFormView: new SearchForm({
+    el: '.search-new',
+    collection: new Items(),
+  }),
+  productView: new ProductView({
+    el: '.cart-item',
+    collection: new CartsItem(),
+  }),
+};
